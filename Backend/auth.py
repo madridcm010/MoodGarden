e794279f0a5c7fcd7b22848c5190a5a855b96492
madridcm010 <snowfallslower@gmail.com>
2026-02-19 23:10:32 -0500

    first push, intial folder setup

---
+++ Backend/auth.py
@@ -0,0 +1,30 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# Fake in-memory database
+fake_users_db = {}
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_user(username: str, password: str):
+    if username in fake_users_db:
+        return False  # user already exists
+
+    fake_users_db[username] = {
+        "username": username,
+        "password": hash_password(password)
+    }
+    return True
+
+def authenticate_user(username: str, password: str):
+    user = fake_users_db.get(username)
+    if not user:
+        return False
+    if not verify_password(password, user["password"]):
+        return False
+    return True
