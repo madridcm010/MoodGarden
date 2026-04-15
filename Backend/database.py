d97683bc2c9ae7301652289f6ffd312dfe8ed3c1
bobby barsh <50975017+Ugotwrcked@users.noreply.github.com>
2026-04-15 02:59:11 +0000

    commit

--- Backend/database.py
+++ Backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://dev:mikealm12@localhost:5432/moodgarden1"
+DATABASE_URL = "sqlite:///./users.db"
 
 engine = create_engine(
     DATABASE_URL,
